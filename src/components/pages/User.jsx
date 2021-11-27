d727f7a884df62b4ccd8e72670597c91d5521e28
Daigoro <48868808+bibimuu@users.noreply.github.com>
2021-11-27 18:59:21 +0900

    atomicDesign

---
+++ src/components/pages/User.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organism/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `加藤${val}`,
+    image: "https://source.unsplash.com/Qb7D1xw28Co",
+    email: "abcdef@gmail.com",
+    phone: "000-0000-0000",
+    company: {
+      name: "Archeco"
+    },
+    website: "http/ssss/s.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>Users</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((val, id) => {
+          return <UserCard keys={id} user={val} />;
+        })}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
