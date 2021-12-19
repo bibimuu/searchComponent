f90646e6ff0606831050353efbd959b0db1087b2
Daigoro <48868808+bibimuu@users.noreply.github.com>
2021-12-19 18:51:09 +0900

    jsonデータをaxiosで取得する

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -6,6 +6,7 @@ import { useSetRecoilState } from "recoil";
 import { userState } from "../../store/userState";
 
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import axios from "axios";
 
 export const Top = () => {
   const history = useHistory();
@@ -21,12 +22,28 @@ export const Top = () => {
     SetUserInfo({ isAdmin: false });
     history.push("/users");
   };
+
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const onClickUser2 = () => {};
+
   return (
     <SContainer>
       <h2>Top</h2>
       <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
       <br />
       <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
+
+      <br />
+      <SecondaryButton onClick={onClickUser1}>あいうえお</SecondaryButton>
+      <SecondaryButton onClick={onClickUser2}>かきくけこ</SecondaryButton>
     </SContainer>
   );
 };
